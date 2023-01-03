25a5b0b96f71a60fece21233d653bf0ed716eb6b
garri87 <pfloresphoto@hotmail.com>
2023-01-03 17:34:02 -0300

    .

--- src/config.py
+++ src/config.py
@@ -25,17 +25,17 @@ class DevelopmentConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''
-    MYSQL_DB = 'zcavengerdb'
+    MYSQL_HOST = os.environ['MYSQL_HOST']
+    MYSQL_USER = os.environ['MYSQL_USER']
+    MYSQL_PASSWORD = os.environ['MYSQL_PASSWORD']
+    MYSQL_DB = os.environ['MYSQL_DB']
     
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USERNAME = 'garri87games@gmail.com'
-    MAIL_PASSWORD = 'tkhaftqwjpyotshj'
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True    
+    MAIL_SERVER = os.environ['MAIL_SERVER']
+    MAIL_PORT = os.environ['MAIL_PORT']
+    MAIL_USERNAME = os.environ['MAIL_USERNAME']
+    MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
+    MAIL_USE_TLS = os.environ['MAIL_USE_TLS']
+    MAIL_USE_SSL = os.environ['MAIL_USE_SSL']    
     
 config = {
     'development': DevelopmentConfig,
