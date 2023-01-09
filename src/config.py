ad7b2d0c4c27cfad443679f6a1d0ecca57328f58
garri87 <pfloresphoto@hotmail.com>
2023-01-09 12:13:49 -0300

    .

--- src/config.py
+++ src/config.py
@@ -28,10 +28,12 @@ class ProductionConfig(Config):
     
     UPLOADS_FOLDER = os.path.join('uploads')
     
-    MYSQL_HOST = os.environ['MYSQL_HOST']
-    MYSQL_USER = os.environ['MYSQL_USER']
-    MYSQL_PASSWORD = os.environ['MYSQL_PASSWORD']
-    MYSQL_DB = os.environ['MYSQL_DB']
+    MYSQL_URL = os. environ['MYSQL_URL']
+    MYSQL_DB = os.environ['MYSQLDATABASE']
+    MYSQL_HOST = os.environ['MYSQLHOST']
+    MYSQL_USER = os.environ['MYSQLUSER']
+    MYSQL_PASSWORD = os.environ['MYSQLPASSWORD']
+    MYSQL_PORT = os.environ['MYSQLPORT']
     
     MAIL_SERVER = os.environ['MAIL_SERVER']
     MAIL_PORT = os.environ['MAIL_PORT']
