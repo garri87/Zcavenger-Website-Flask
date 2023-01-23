d58b5fbbcb275afa5d3acadc4510d0df0b3c268d
garri87 <pfloresphoto@hotmail.com>
2023-01-23 11:40:06 -0300

    Update production
    Change contact form method

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
 
+import requests
+
 from config import config,DATABASE_CONNECTION_URI,SQLALCHEMY_TRACK_MODIFICATIONS
 
 from flask_login import LoginManager, login_required
 
-from flask_mail import Mail
+from flask_mail import Mail, Message
   
 from models.ModelUser import ModelUser
 
@@ -42,8 +44,8 @@ app.register_blueprint(forum)
 @app.route('/')
 @app.route('/index') 
 def index():
-        
-    return render_template('index.html')
+    site_key = config('RECAPTCHA_SITE_KEY')    
+    return render_template('index.html', site_key = site_key)
 
 @app.route('/development')
 def development():
@@ -56,6 +58,36 @@ def development():
 def home(): 
     
     return render_template('home.html')
+
+
+@app.route('/contactForm', methods=['GET', 'POST'])
+def contacForm():
+    if request.method == 'POST':
+        
+        secret_response = request.form['g-recaptcha-response']
+        
+        verify_response = requests.post(url=f"{config('RECAPTCHA_VERIFY_URL')}?secret={config('RECAPTCHA_SECRET_KEY')}&response={secret_response}").json()
+             
+        if verify_response["success"] == True:
+            _name = request.form['name']
+            _email = request.form['email']
+            _text = request.form['text']
+            
+            msg = Message('Zcavenger.com | New message', sender = 'noreply@zcavenger.com', recipients= ['garri87games@gmail.com'])
+
+            msg.body = ""
+            msg.html = render_template('contactMessage.html', 
+                                       name = _name, 
+                                       email = _email, 
+                                       text = _text)
+            mail.send(msg)
+            
+            flash("Thank you, " + _name + " your message was received correctly")
+            return redirect(url_for('index')) 
+        else:
+            print("Error")
+            return redirect(url_for('index'))  
+
     
 @app.route('/uploads/<fileName>')
 def uploads(fileName):
